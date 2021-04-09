2843f1a65108580421aadce451e405116711d56e
Aakriti Sahrawat <51236131+aaksah1234@users.noreply.github.com>
2021-04-10 00:40:39 +0530

    explanations-1

--- recover.py
+++ recover.py
@@ -1,4 +1,22 @@
 import pandas as pd
+
+"""
+Pandas is an open-source Python Library providing high-performance data manipulation and analysis tool using its powerful data structures. The name Pandas is derived from the word Panel Data – an Econometrics from Multidimensional data.
+Key features:
+
+-Fast and efficient DataFrame object with default and customized indexing.
+-Tools for loading data into in-memory data objects from different file formats.
+-Data alignment and integrated handling of missing data.
+-Reshaping and pivoting of date sets.
+-Label-based slicing, indexing and subsetting of large data sets.
+-Columns from a data structure can be deleted or inserted.
+-Group by data for aggregation and transformations.
+-High performance merging and joining of data.
+-Time Series functionality.
+
+"""
+
+
 import codecs
 import os
 import sys
